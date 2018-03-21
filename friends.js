07890d03aa5aee7da0d3cb9a97069377ea9f3734
rebecca8840 <31925939+rebecca8840@users.noreply.github.com>
2018-03-21 16:52:48 -0400

    Add files via upload

---
+++ friends.js
@@ -0,0 +1,44 @@
+var friends = friendsArray =[
+{
+	name: "David Bowie",
+	photo:"https://upload.wikimedia.org/wikipedia/commons/2/2c/David_Bowie_-_TopPop_1974_08.png",
+	scores:["3", "2", "1", "3", "2", "3", "4", "2", "5", "2"]
+},
+{
+	name:"Marie Antoinette",
+	photo:"http://www.imdb.com/title/tt0422720/mediaviewer/rm789157888",
+	scores:["5", "2", "2", "1", "2", "2", "3", "2", "4", "4"]
+},
+{
+	name:"Lady Gaga",
+	photo:"https://upload.wikimedia.org/wikipedia/commons/0/0e/Lady_Gaga_Glitter_and_Grease2.jpg",
+	scores:["5", "3", "3", "1", "3", "2", "5", "3", "4", "3"]
+},
+{
+	name:"Freddie Mercury",
+	photo:"https://upload.wikimedia.org/wikipedia/commons/e/ef/Freddie_Mercury_performing_in_New_Haven%2C_CT%2C_November_1977.jpg",
+	scores:["5", "2", "3", "1", "3", "2", "4", "2", "4", "2"]
+},
+{
+	name:"Anna Pavlova",
+	photo:"https://upload.wikimedia.org/wikipedia/commons/4/43/Pharaoh%27s_Daughter_-Anna_Pavlova_-1910.jpg",
+	scores:["2", "5", "4", "4", "3", "3", "5", "4", "3", "2"]
+},
+{
+	name:"Bianca Jagger",
+	photo:"https://www.harpersbazaar.com/culture/features/g5648/bianca-jagger-style/?slide=5",
+	scores:["4", "4", "4", "2", "3", "2", "5", "4", "5", "4"]
+},
+{
+	name:"Anna May Wong",
+	photo:"https://upload.wikimedia.org/wikipedia/commons/8/83/Anna_May_Wong_-_portrait.jpg",
+	scores:["3", "3", "4", "3", "2", "2", "5", "4", "4", "2"]
+},
+{
+	name:"SZA",
+	photo:"https://www.nytimes.com/2018/01/23/arts/music/sza-ctrl-grammy-awards.html",
+	scores:["5", "2", "4", "1", "3", "3", "5", "3", "4", "2"]
+}
+]
+;
+
